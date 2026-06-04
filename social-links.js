cd227499c119b9ccbac3ad58afc9bb21353424c9
amaanking806 <theinfomaacademy@gmail.com>
2026-06-04 15:00:01 +0530

    Update social media links in social-links.js

--- social-links.js
+++ social-links.js
@@ -7,9 +7,9 @@ const CONFIG = {
   // --- Social Media Links ---
   social: {
     instagram: "https://instagram.com/infoma.in",
-    facebook:  "https://facebook.com/infoma.in",
-    twitter:   "https://twitter.com/infoma.in",
-    linkedin:  "https://linkedin.com/company/infoma"
+    facebook:  "https://www.facebook.com/share/1CnGDkvd3G/",
+    twitter:   "https://x.com/InfomaAcademy",
+    linkedin:  "https://www.linkedin.com/in/amaan-shakeel-4827553b7/"
   },
 
   // --- Navigation Links ---
